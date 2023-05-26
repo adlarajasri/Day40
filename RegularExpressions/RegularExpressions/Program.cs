24eab15d868ef80d415af156f4df342010189250
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-05-26 14:27:32 +0530

    Add files via upload

---
+++ RegularExpressions/RegularExpressions/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string[] strings = new string[n];
+
+            for (int i = 0; i < n; i++)
+
+            {
+
+                strings[i] = Console.ReadLine();
+
+            }
+
+            foreach (string str in strings)
+
+            {
+
+                string result = RemoveDuplicateWords(str);
+
+                Console.WriteLine(result);
+
+            }
+
+        }
+
+        static string RemoveDuplicateWords(string sentence)
+
+        {
+
+            string pattern = @"\b(\w+)\b\s+(?=.*\b\1\b)";
+
+            string rep = "";
+
+            Regex regex = new Regex(pattern);
+
+            string result = regex.Replace(sentence, rep);
+
+            return result;
+        }
+    }
+}
