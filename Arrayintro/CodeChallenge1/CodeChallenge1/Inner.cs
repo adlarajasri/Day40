ed3866697324a3214660078e7b6af9e7479326d8
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-05-26 14:26:57 +0530

    Add files via upload

---
+++ Arrayintro/CodeChallenge1/CodeChallenge1/Inner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge1
+{
+    class Inner
+    {
+        public class Private
+
+        {
+
+            public bool Power(int n)
+
+            {
+
+                if (n == 0)
+
+                    return false;
+
+                while (n != 1)
+
+                {
+
+                    if (n % 2 != 0)
+
+                        return false;
+
+                    n = n / 2;
+
+                }
+
+                return true;
+
+            }
+        }
+    }
+}
---
+++ Arrayintro/CodeChallenge1/CodeChallenge1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Inner.Private innerprivate = new Inner.Private();
+
+            Console.WriteLine("Enter the number:");
+
+            int n = int.Parse(Console.ReadLine());
+
+            if (innerprivate.Power(n))
+
+            {
+
+                Console.WriteLine(n + " is power of 2");
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine(n + " is not power of 2");
+
+            }
+
+        }
+    }
+    
+}
